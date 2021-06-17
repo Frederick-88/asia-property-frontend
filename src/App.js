c0b7b0e11e7878bbe7344bb1caaec3b01808b805
Chen Frederick <frederickfd88@gmail.com>
2021-06-17 22:00:04 +0700

    Setup Router to Separate Pages Based on URL

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserView, MobileView } from "react-device-detect";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import InitialPage from "./components/index";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserView>
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <InitialPage />
+            </Route>
+          </Switch>
+        </Router>
+      </BrowserView>
+      <MobileView>
+        <h2>
+          Not Rendered on Mobile, please visit from desktop/bigger screen.
+        </h2>
+      </MobileView>
     </div>
   );
 }
---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+const Index = () => {
+  return (
+    <div>
+      <h2> Hi There </h2>
+    </div>
+  );
+};
+
+export default Index;
