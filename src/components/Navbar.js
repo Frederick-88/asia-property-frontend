ae953343cfff2c35ed9b2dd9fe8fabdfebd69350
Chen Frederick <frederickfd88@gmail.com>
2021-07-02 22:44:56 +0700

    Done Navbar UI

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,40 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Logo from "../assets/images/logo.png";
 import "../assets/styles/navbar.scss";
 
 const Navbar = () => {
   return (
     <section className="navigation__bar">
-      <div>
-        <Link to="/" className="nav__item main-nav">
-          Asia Property
+      <div className="navigation__list">
+        <div className="nav-image__wrapper">
+          <img className="nav__image" src={Logo} alt="logo" />
+          <h4 className="nav__logo-title">Asia Property</h4>
+        </div>
+        <Link to="/" className="nav__item">
+          Listing
         </Link>
-        <hr className="nav__link" />
+        <Link to="/" className="nav__item">
+          Favourites
+        </Link>
+        <Link to="/" className="nav__item">
+          Our Agents
+        </Link>
+        <Link to="/" className="nav__item">
+          About Us
+        </Link>
       </div>
-      <Link to="/" className="nav__item">
-        Property
-      </Link>
-      <Link to="/" className="nav__item">
-        Property
-      </Link>
-      <Link to="/" className="nav__item">
-        Property
-      </Link>
-      <Link to="/" className="nav__item">
-        Property
-      </Link>
+
+      <div className="auth-button__list">
+        <button type="button" className="nav__auth-button">
+          <i className="icon-user icon" />
+          <p className="title">Sign in</p>
+        </button>
+        <button type="button" className="nav__auth-button auth--admin">
+          <p className="title">For Admin</p>
+          <i className="icon-arrow-right icon" />
+        </button>
+      </div>
     </section>
   );
 };
