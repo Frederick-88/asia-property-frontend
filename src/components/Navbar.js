b6a999ccef325053d0a578868a75f1927615d9f0
Chen Frederick <frederickfd88@gmail.com>
2021-07-03 20:00:45 +0700

    Updated Navbar into Class Component & Added Scroll Support

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,42 +1,74 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import Logo from "../assets/images/logo.png";
 import "../assets/styles/navbar.scss";
 
-const Navbar = () => {
-  return (
-    <section className="navigation__bar">
-      <div className="navigation__list">
-        <div className="nav-image__wrapper">
-          <img className="nav__image" src={Logo} alt="logo" />
-          <h4 className="nav__logo-title">Asia Property</h4>
+export default class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isScrolled: false,
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll);
+  }
+
+  handleScroll = () => {
+    const position = document.documentElement;
+    const scrolledThroughLimit = position.scrollTop > 120;
+    this.setState({ isScrolled: scrolledThroughLimit });
+  };
+
+  navClass = () => {
+    const classArray = ["navigation__bar"];
+    if (this.state.isScrolled) {
+      classArray.push("nav--scrolled");
+    }
+
+    return classArray.join(" "); // creating result like `navigation__bar nav--scrolled` so usable in react's className
+  };
+
+  render() {
+    const navClass = this.navClass();
+
+    return (
+      <section className={navClass}>
+        <div className="navigation__list">
+          <div className="nav-image__wrapper">
+            <img className="nav__image" src={Logo} alt="logo" />
+            <h4 className="nav__logo-title">Asia Property</h4>
+          </div>
+          <Link to="/" className="nav__item">
+            Listing
+          </Link>
+          <Link to="/" className="nav__item">
+            Wishlists
+          </Link>
+          <Link to="/" className="nav__item">
+            Our Agents
+          </Link>
+          <Link to="/" className="nav__item">
+            About Us
+          </Link>
         </div>
-        <Link to="/" className="nav__item">
-          Listing
-        </Link>
-        <Link to="/" className="nav__item">
-          Favourites
-        </Link>
-        <Link to="/" className="nav__item">
-          Our Agents
-        </Link>
-        <Link to="/" className="nav__item">
-          About Us
-        </Link>
-      </div>
 
-      <div className="auth-button__list">
-        <button type="button" className="nav__auth-button">
-          <i className="icon-user icon" />
-          <p className="title">Sign in</p>
-        </button>
-        <button type="button" className="nav__auth-button auth--admin">
-          <p className="title">For Admin</p>
-          <i className="icon-arrow-right icon" />
-        </button>
-      </div>
-    </section>
-  );
-};
-
-export default Navbar;
+        <div className="auth-button__list">
+          <button type="button" className="nav__auth-button">
+            <i className="icon-user icon" />
+            <p className="title">Sign in</p>
+          </button>
+          <button type="button" className="nav__auth-button auth--admin">
+            <p className="title">For Admin</p>
+            <i className="icon-arrow-right icon" />
+          </button>
+        </div>
+      </section>
+    );
+  }
+}
