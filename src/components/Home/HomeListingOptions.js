d70f7d499d5069339a796e35c0b7aaedb8cf2e71
Chen Frederick <frederickfd88@gmail.com>
2021-07-04 21:54:41 +0700

    Added HomeListingOptions Page

---
+++ src/components/Home/HomeListingOptions.js
@@ -0,0 +1,66 @@
+import React from "react";
+import ApartmentImage from "../../../src/assets/images/sample-apartment.jpg";
+import HouseImage from "../../../src/assets/images/sample-house.jpg";
+import HomeClusterImage from "../../../src/assets/images/sample-home-cluster.jpg";
+import VillaImage from "../../../src/assets/images/sample-villa.jpg";
+
+const HomeListingOptions = () => {
+  const options = [
+    {
+      title: "Apartment",
+      image: ApartmentImage,
+      listing_count: 52,
+    },
+    {
+      title: "House",
+      image: HouseImage,
+      listing_count: 88,
+    },
+    {
+      title: "Home Cluster",
+      image: HomeClusterImage,
+      listing_count: 7,
+    },
+    {
+      title: "Villa",
+      image: VillaImage,
+      listing_count: 0,
+    },
+  ];
+
+  return (
+    <section className="home__listing-options">
+      <div className="options__header-wrapper">
+        <h4 className="header-title">Browse listings by category</h4>
+        <p className="header-subtitle">
+          Highlight the best of your properties by using the List Category
+          shortcode. You can list categories, types, cities, areas and states.
+        </p>
+      </div>
+
+      <div className="options__list-wrapper">
+        {options.map((option) => {
+          return (
+            <button
+              type="button"
+              key={option.title}
+              className="options__list-item"
+            >
+              <img
+                className="item-image"
+                src={option.image}
+                alt="real-estate-option"
+              />
+              <h4 className="item-title">{option.title}</h4>
+              <h4 className="item__listing-count">
+                {option.listing_count} Listings
+              </h4>
+            </button>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default HomeListingOptions;
