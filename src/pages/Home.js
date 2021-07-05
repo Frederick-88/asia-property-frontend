80b43d3343578cf170f68c3a5a0ffff9d76d5981
Chen Frederick <frederickfd88@gmail.com>
2021-07-05 20:41:02 +0700

    Simplify Slick-Carousel CSS Import in Home-Page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,11 @@ import "../assets/styles/home.scss";
 import HomeBanner from "../components/Home/HomeBanner";
 import HomeListingPreview from "../components/Home/HomeListingPreview";
 import HomeListingOptions from "../components/Home/HomeListingOptions";
+import HomeDevelopers from "../components/Home/HomeDevelopers";
+import HomeAgents from "../components/Home/HomeAgents";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Home = () => {
   return (
@@ -12,6 +17,8 @@ const Home = () => {
       <HomeListingPreview />
       <HomeListingOptions />
       <HomeListingPreview type="rent" />
+      <HomeDevelopers />
+      <HomeAgents />
     </div>
   );
 };
