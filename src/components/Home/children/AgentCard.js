ff2c33c5cd48f209b285c7678df2cf9e238059f7
Chen Frederick <frederickfd88@gmail.com>
2021-07-06 22:01:58 +0700

    Updated Icons in AgentCard

--- src/components/Home/children/AgentCard.js
+++ src/components/Home/children/AgentCard.js
@@ -10,15 +10,15 @@ const AgentCard = (props) => {
       <div className="card__content">
         <h4 className="title">{props.data.name}</h4>
         <div className="card-detail detail--location">
-          <i className="icon-travel detail-icon" />
+          <i className="icon-location detail-icon" />
           <p className="detail-text">{locationText}</p>
         </div>
         <div className="card-detail">
-          <i className="icon-travel detail-icon" />
+          <i className="icon-phone detail-icon" />
           <p className="detail-text">{props.data.phone_number}</p>
         </div>
         <div className="card-detail">
-          <i className="icon-travel detail-icon" />
+          <i className="icon-mail detail-icon" />
           <p className="detail-text">{props.data.email}</p>
         </div>
       </div>
