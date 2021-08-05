3d2da7f6bfc7f8262894dd2e49974489019834b2
Chen Frederick <frederickfd88@gmail.com>
2021-08-05 22:39:53 +0700

    Created Admin Users

---
+++ src/components/Admin/AdminUsersComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AdminUsersComponent = (props) => {
+  return (
+    <div>
+      <h4>Admin Users here</h4>
+    </div>
+  );
+};
+
+export default AdminUsersComponent;
---
+++ src/pages/admin/AdminUsers.js
@@ -0,0 +1,12 @@
+import React from "react";
+import AdminUsersComponent from "../../components/Admin/AdminUsersComponent";
+
+const AdminUsers = (props) => {
+  return (
+    <div className="admin-listing__container">
+      <AdminUsersComponent />
+    </div>
+  );
+};
+
+export default AdminUsers;
