c82b8710b3df295561ea1d152a791393df240370
Chen Frederick <frederickfd88@gmail.com>
2021-08-05 22:39:30 +0700

    Created Admin Listing

---
+++ src/components/Admin/AdminListingComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AdminListingComponent = (props) => {
+  return (
+    <div>
+      <h4>Admin listing here</h4>
+    </div>
+  );
+};
+
+export default AdminListingComponent;
---
+++ src/pages/admin/AdminListing.js
@@ -0,0 +1,12 @@
+import React from "react";
+import AdminListingComponent from "../../components/Admin/AdminListingComponent";
+
+const AdminListing = (props) => {
+  return (
+    <div className="admin-listing__container">
+      <AdminListingComponent />
+    </div>
+  );
+};
+
+export default AdminListing;
