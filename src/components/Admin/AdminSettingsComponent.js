39e79475cab39255af60bb9bee23d0f2f1a60d9f
Chen Frederick <frederickfd88@gmail.com>
2021-08-05 22:39:43 +0700

    Created Admin Settings

---
+++ src/components/Admin/AdminSettingsComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AdminSettingsComponent = (props) => {
+  return (
+    <div>
+      <h4>Admin settings here</h4>
+    </div>
+  );
+};
+
+export default AdminSettingsComponent;
---
+++ src/pages/admin/AdminSettings.js
@@ -0,0 +1,12 @@
+import React from "react";
+import AdminSettingsComponent from "../../components/Admin/AdminSettingsComponent";
+
+const AdminSettings = (props) => {
+  return (
+    <div className="admin-listing__container">
+      <AdminSettingsComponent />
+    </div>
+  );
+};
+
+export default AdminSettings;
