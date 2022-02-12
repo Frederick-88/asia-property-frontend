12b25e6294f6165bdd7384cba0151a106a4e372b
Chen Frederick <frederickfd88@gmail.com>
2022-02-12 21:37:37 +0700

    Updated Admin Sidebar

--- src/components/Admin/AdminSidebar.js
+++ src/components/Admin/AdminSidebar.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import Logo from "../../assets/images/logo.png";
-import SampleAdmin from "../../assets/images/sample-user.png";
 
 const AdminSidebar = (props) => {
     const [activeButton, setActiveButton] = useState("dashboard");
@@ -51,33 +50,44 @@ const AdminSidebar = (props) => {
 
     return (
         <div className='admin-sidebar'>
-            <div className='sidebar__logo'>
-              <Link to="/admin" className="sidebar__logo">
-                <img className='logo' src={Logo} alt="logo"/>
-                <h4 className="logo-title">Asia Property</h4>
-              </Link>
+            <div className="sidebar__content">
+                <div className='sidebar__logo'>
+                <Link to="/admin" className="sidebar__logo">
+                    <img className='logo' src={Logo} alt="logo"/>
+                    <h4 className="logo-title">Asia Property</h4>
+                </Link>
+                </div>
+
+                <div className='sidebar__user-profile'>
+                    <img className='profile-image' src="https://pbs.twimg.com/media/FJYRgy6acAYzXgj.jpg" alt="logo"/>
+                    <h4 className='profile-title'>Frederick</h4>
+                    <p className='profile-role'>Admin</p>
+                </div>
+
+                <div className='sidebar__button-list'>
+                    {sidebarButtonList.map((button, index) => {
+                        return (
+                            <button 
+                                type='button' 
+                                className={'sidebar-button ' + isActiveButton(button.value)} 
+                                key={index}
+                                onClick={() => selectButton(button.value)}
+                            >
+                                <i className={'button-icon ' + button.class} />
+                                <p className="button-text">{button.name}</p>
+                            </button>
+                        );
+                    })}
+                </div>
             </div>
 
-            <div className='sidebar__user-profile'>
-                <img className='profile-image' src={SampleAdmin} alt="logo"/>
-                <h4 className='profile-title'>Frederick</h4>
-                <p className='profile-role'>Admin</p>
-            </div>
-
-            <div className='sidebar__button-list'>
-                {sidebarButtonList.map((button, index) => {
-                    return (
-                        <button 
-                            type='button' 
-                            className={'sidebar-button ' + isActiveButton(button.value)} 
-                            key={index}
-                            onClick={() => selectButton(button.value)}
-                        >
-                            <i className={'button-icon ' + button.class} />
-                            <p className="button-text">{button.name}</p>
-                        </button>
-                    );
-                })}
+            <div className="sidebar__reminder-box">
+                <div className="sidebar__reminder-content">
+                    <i className="icon-shift" />
+                    <i className="content-icon icon-speedometer" />
+                    <h4 className="title">History Available</h4>
+                    <p className="subtitle">Check your weekly transaction reports</p>
+                </div>
             </div>
         </div>
     )
