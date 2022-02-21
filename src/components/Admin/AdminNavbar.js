e32e2da653bfcedc1024126179a2bd2e67f5b70c
Chen Frederick <frederickfd88@gmail.com>
2022-02-21 22:28:32 +0700

    Admin Site - Done Admin Navbar Component

--- src/components/Admin/AdminNavbar.js
+++ src/components/Admin/AdminNavbar.js
@@ -1,35 +1,132 @@
-import React from 'react';
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { toast } from "react-toastify";
+
+import AboutUsModal from "../AboutUsModal";
+import AccountSettingsModal from "../AccountSettingsModal";
+import AdminProfileModal from "./children/AdminProfileModal";
 
 const AdminNavbar = (props) => {
-    const navIcons = ['icon-notification-line', 'icon-message', 'icon-gift', 'icon-user-line']
-    const handleSearchInput = (event) => {
-        let { value } = event.currentTarget;
-        // setSearchQueryInput(value);
-        console.log('search value', value);
-    };
+  const history = useHistory();
+  const navbarButtonList = [
+    {
+      icon: "icon-notification-line",
+      type: "notification",
+      additionalClass: "",
+    },
+    {
+      icon: "icon-message",
+      type: "chat",
+      additionalClass: "",
+    },
+    {
+      icon: "icon-gift",
+      type: "gift",
+      additionalClass: "",
+    },
+    {
+      icon: "icon-user-line",
+      type: "profile",
+      additionalClass: "button--profile",
+    },
+  ];
 
-    return (
-        <div className='admin-navbar'>
-            <div className='navbar__search-bar'>
-                <i className="icon-search search-icon" />
-                <input
-                    className="search-input"
-                    placeholder="Search"
-                    onChange={(event) => handleSearchInput(event)}
-                />
-            </div>
+  const [isProfileModalShow, setIsProfileModalShow] = useState(false);
+  const [isAccountSettingsModalShow, setIsAccountSettingsModalShow] =
+    useState(false);
+  const [isAboutUsModalShow, setIsAboutUsModalShow] = useState(false);
 
-            <div className='navbar__button-list'>
-                {navIcons.map((iconClass, index) => {
-                    return (
-                        <button type='button' className='navbar-button' key={index}>
-                            <i className={iconClass} />
-                        </button>
-                    );
-                })}
-            </div>
-        </div>
-    )
+  // ----------------------
+  //  < ---- Methods ---- >
+  // ----------------------
+
+  const handleSearchKeyDown = (event) => {
+    if (event.key === "Enter") comingSoonNotification("Global search");
+  };
+
+  const onClickNavbarButton = (type) => {
+    const capitalizeType = type.charAt(0).toUpperCase() + type.slice(1);
+    if (type !== "profile") comingSoonNotification(capitalizeType);
+    else toggleAdminUserProfileModal();
+  };
+
+  const onClickAdminProfileModalButton = (type) => {
+    if (type === "about-developer") setAboutUsModal(true);
+    else if (type === "staff") history.push("/admin/users");
+    else if (type === "account-settings") setAccountSettingsModal(true);
+    else console.log("clicked profile modal type =", type);
+
+    toggleAdminUserProfileModal();
+  };
+
+  const toggleAdminUserProfileModal = () => {
+    setIsProfileModalShow(!isProfileModalShow);
+  };
+
+  const hideIsProfileModalShow = () => {
+    setIsProfileModalShow(false);
+  };
+
+  const setAccountSettingsModal = (boolean) => {
+    setIsAccountSettingsModalShow(boolean);
+  };
+
+  const setAboutUsModal = (boolean) => {
+    setIsAboutUsModalShow(boolean);
+  };
+
+  const comingSoonNotification = (featureName) => {
+    toast.success(`${featureName} feature will come soon.`, {
+      position: toast.POSITION.TOP_CENTER,
+      autoClose: 5000,
+    });
+  };
+
+  return (
+    <div className="admin-navbar">
+      <div className="navbar__search-bar">
+        <i className="icon-search search-icon" />
+        <input
+          className="search-input"
+          placeholder="Search"
+          onKeyDown={(event) => handleSearchKeyDown(event)}
+        />
+      </div>
+
+      <div className="navbar__button-list">
+        {navbarButtonList.map((button, index) => {
+          return (
+            <button
+              type="button"
+              className={"navbar-button " + button.additionalClass}
+              key={index}
+              onClick={() => onClickNavbarButton(button.type)}
+            >
+              <i className={button.icon} />
+            </button>
+          );
+        })}
+      </div>
+
+      <AdminProfileModal
+        isProfileModalShow={isProfileModalShow}
+        hideIsProfileModalShow={hideIsProfileModalShow}
+        onClickAdminProfileModalButton={onClickAdminProfileModalButton}
+      />
+
+      <AccountSettingsModal
+        isAdmin={true}
+        isAccountSettingsModalShow={isAccountSettingsModalShow}
+        hideModal={() => setAccountSettingsModal(false)}
+      />
+
+      <AboutUsModal
+        isDark={true}
+        isAboutUsModalShow={isAboutUsModalShow}
+        hideModal={() => setAboutUsModal(false)}
+      />
+    </div>
+  );
 };
 
 export default AdminNavbar;
