4759e18b34b889f22561c173e2d3612be80a53c7
Chen Frederick <frederickfd88@gmail.com>
2022-03-01 22:28:31 +0700

    React Redux - Added Admin Reducer to Global Redux Store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from "redux";
 
 import LoginReducer from "./LoginReducer";
 import UsersReducer from "./UsersReducer";
+import AdminReducer from "./AdminReducer";
 
 const combineReducer = combineReducers({
   LoginReducer,
   UsersReducer,
+  AdminReducer,
 });
 
 export default combineReducer;
