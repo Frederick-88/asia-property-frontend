8ca8f577e3a11b8e24bc73e7bdbf308421e9e345
Chen Frederick <frederickfd88@gmail.com>
2022-03-01 22:28:48 +0700

    React Redux - Added Admin ActionCreators, Initial Setup

---
+++ src/actionCreators/AdminAction.js
@@ -0,0 +1,13 @@
+// import axios from "axios";
+// import jwt from "jwt-decode";
+
+// const apiURL = "https://asia-property.herokuapp.com";
+
+// export const setAdminSitePersonRole = (item) => {
+//   return (dispatch) => {
+//     dispatch({
+//       type: "SET_ADMIN_SITE_USER_ROLE",
+//       payload: item,
+//     });
+//   };
+// };
