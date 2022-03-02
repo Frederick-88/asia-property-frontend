3756c2d0870c0b46f6da9932e70bde5a2ea287b8
Chen Frederick <frederickfd88@gmail.com>
2022-03-02 22:29:32 +0700

    Homepage - User Profile Modal : Added Go To Admin Site Button for Admins

--- src/components/UserProfileModal.js
+++ src/components/UserProfileModal.js
@@ -1,8 +1,10 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import { useDetectClickOutside } from "react-detect-click-outside";
 import { connect } from "react-redux";
 
 const UserProfileModal = (props) => {
+  const history = useHistory();
   const userProfileType = props.userToken ? props.userData : props.adminData;
 
   const userProfileModalClass = () => {
@@ -18,6 +20,10 @@ const UserProfileModal = (props) => {
   const reactClickOutside = useDetectClickOutside({
     onTriggered: hideIsProfileModalShow,
   });
+
+  const goToAdminSite = () => {
+    history.push("/admin");
+  };
 
   const goToAccountSettings = () => {
     props.goToAccountSettings();
@@ -48,7 +54,7 @@ const UserProfileModal = (props) => {
 
       <div className={userProfileModalClass()}>
         <p className="profile-username">{userProfileType.username}</p>
-        <p className="profile-role">{userProfileType.role}</p>
+        <p className="profile-role">{props.userRole}</p>
 
         <div className="profile-container">
           <i className="icon icon-mail" />
@@ -61,6 +67,15 @@ const UserProfileModal = (props) => {
 
         <hr className="divider-line" />
 
+        {props.userRole === "admin" && (
+          <button
+            type="button"
+            className="profile-button"
+            onClick={goToAdminSite}
+          >
+            Go To Admin Site
+          </button>
+        )}
         <button
           type="button"
           className="profile-button"
@@ -89,6 +104,7 @@ const UserProfileModal = (props) => {
 
 const mapStateToProps = (state) => {
   return {
+    userRole: state.LoginReducer.userRole,
     userData: state.LoginReducer.userData,
     adminData: state.LoginReducer.adminData,
     userToken: state.LoginReducer.userToken,
