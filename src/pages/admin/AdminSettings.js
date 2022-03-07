33ddc8b342707a22f704a887471fdb31e334c8c6
Chen Frederick <frederickfd88@gmail.com>
2022-03-07 07:17:58 +0700

    Admin Site - Added AdminSettings Pages

--- src/pages/admin/AdminSettings.js
+++ src/pages/admin/AdminSettings.js
@@ -2,7 +2,13 @@ import React from "react";
 import AdminSettingsComponent from "../../components/Admin/AdminSettingsComponent";
 
 const AdminSettings = (props) => {
-  return <AdminSettingsComponent />
+  const setAccountSettingsModal = (boolean) => {
+    props.setAccountSettingsModal(boolean);
+  };
+
+  return (
+    <AdminSettingsComponent setAccountSettingsModal={setAccountSettingsModal} />
+  );
 };
 
 export default AdminSettings;
