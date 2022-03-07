2bc489e352ff1d08d34c0c9faf905c9e29c4ccaa
Chen Frederick <frederickfd88@gmail.com>
2022-03-07 07:18:11 +0700

    Admin Site - Added AdminUsers Pages

--- src/pages/admin/AdminUsers.js
+++ src/pages/admin/AdminUsers.js
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
 import AdminUsersComponent from "../../components/Admin/AdminUsersComponent";
 
+import { getUsers } from "../../actionCreators/AdminAction";
+
 const AdminUsers = (props) => {
-  return <AdminUsersComponent />
+  const setDeleteModalObjData = (data) => {
+    props.setDeleteModalObjData(data);
+  };
+
+  useEffect(() => {
+    if (!props.usersData.length) props.getUsers();
+  }, [props]);
+
+  return <AdminUsersComponent setDeleteModalObjData={setDeleteModalObjData} />;
 };
 
-export default AdminUsers;
+const mapStateToProps = (state) => {
+  return {
+    usersData: state.AdminReducer.usersData,
+  };
+};
+
+const mapDispatchToProps = {
+  getUsers,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(AdminUsers);
